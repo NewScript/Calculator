43ec18827299b19b95b4a891950bd51a0d9a8b85
Carlos Timoteo Arruda <carlos.t.arruda@gmail.com>
2021-07-18 15:47:10 -0300

    allClear clear e BackSpace funcional, apresentacao e numeros inteiros e decimais

--- scripts/index.js
+++ scripts/index.js
@@ -30,7 +30,7 @@ let $strDecimal = '';
 let $strSignal = '';
 let $flagComma ={
     strComma:'',
-    numIndexComma: 0
+    strIndexComma: ''
 };
 let $strFullNumber = '0';
 
@@ -43,7 +43,7 @@ function clearValues(){
     $strDecimal = '';
     $strSignal = '';
     $flagComma.strComma = '';
-    $flagComma.numIndexComma = null;
+    $flagComma.strIndexComma = '';
     $strFullNumber = '';
     $numNumber = 0;
 }
@@ -62,20 +62,26 @@ function presentNumber(){
         
 };
 
-function presentFormulation(){
+function presentFormulation(e){
+    if(viewCalculation.calculationFormulation.textContent === '0'){
+        viewCalculation.calculationFormulation.textContent = '';
+    }
     if($strInteger != 0){
-        viewCalculation.calculationFormulation.textContent = $strFullNumber;
+        viewCalculation.calculationFormulation.textContent += `${$strFullNumber} ${e.target.textContent} `;
+        clear();
     }else{
         viewCalculation.calculationFormulation.textContent = '0';
-    } 
+    };
 };
 
 function storeNumber(strNumber){
-    if (!$flagComma.strComma) {
-        $strInteger += strNumber;
-    }else{
-        $strDecimal += strNumber;
-    };
+    if(!($strInteger === '' && strNumber === '0')){
+        if (!$flagComma.strComma) {
+            $strInteger += strNumber;
+        }else{
+            $strDecimal += strNumber;
+        };
+    }
 };
 
 function clear(){
@@ -104,9 +110,62 @@ function changeSign(){
     if(!$strSignal){
         $strSignal = '-';
         presentNumber();
+        calculateCommaIndex();
     }else{
         $strSignal = '';
         presentNumber();
+        calculateCommaIndex();
+    };
+};
+
+function includeComma() {
+    if (!$flagComma.strComma) {
+        $flagComma.strComma = ',';
+        presentNumber();
+        calculateCommaIndex();
+    };
+};
+
+function calculateCommaIndex(){
+    if($flagComma.strComma){
+        $flagComma.strIndexComma = $strSignal.length + $strInteger.length;
+    }else{
+        $flagComma.strIndexComma = '';
+    };
+};
+
+function bsPresentNumber(){
+    
+    let indexOfTheCharacterToBeDeleted = ((viewCalculation.numberPresentation.textContent).length - 1);
+    let letterToErase = viewCalculation.numberPresentation.textContent.charAt(indexOfTheCharacterToBeDeleted);
+    viewCalculation.numberPresentation.textContent = (viewCalculation.numberPresentation.textContent).slice(0, indexOfTheCharacterToBeDeleted);
+    
+    if(indexOfTheCharacterToBeDeleted === 0){
+        viewCalculation.numberPresentation.textContent = '0';
+        maximumCharactersAllowed(0);
+    };
+
+    if(letterToErase != ',' || letterToErase != '-' || letterToErase != '.'){
+        bsStoreNumber();
+    }
+
+    if(letterToErase === ','){
+        $flagComma.strComma = '';
+        calculateCommaIndex();
+    };
+    if(letterToErase === '-'){
+        $strSignalValue = '';
+    };
+
+};
+
+function bsStoreNumber(){
+    if($flagComma.strIndexComma){
+        $strDecimal = $strDecimal.slice(0, $strDecimal.length - 1);
+        // console.log('Decimal = ' + $strDecimal + ' - ' + $strDecimal.length);
+    }else{
+        $strInteger = $strInteger.slice(0, $strInteger.length - 1);
+        // console.log('Integer = ' + $strInteger + ' - ' + $strInteger.length);
     };
 };
 
@@ -120,4 +179,18 @@ for (const iterator of operand) {
     iterator.addEventListener('click', includeNumber);
 };
 
+action.backSpace.addEventListener('click', bsPresentNumber);
+
+operator.sum.addEventListener('click', presentFormulation);
+
+operator.subtraction.addEventListener('click', presentFormulation);
+
+operator.division.addEventListener('click', presentFormulation);
+
+operator.multiplication.addEventListener('click', presentFormulation);
+
+operator.result.addEventListener('click', presentFormulation);
+
 signalChange.addEventListener('click', changeSign);
+
+comma.addEventListener('click', includeComma);
